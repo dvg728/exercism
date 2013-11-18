8742103ad318f10b52457a6aa00228471c0fefd9
Jonah <Jonahkmoses@gmail.com>
2013-11-17 21:16:03 -0700

    added methods to private methods and adjusted names

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -7,20 +7,26 @@ class Anagram
 
   def match(possible_anagrams)
     possible_anagrams.select do |possible_anagram|
-      words?(possible_anagram)
+      valid_anagram?(possible_anagram)
     end
   end
 
-  def words?(possible_anagram)
-    !matched_against_words?(possible_anagram) && anagram_match?(possible_anagram)
+private
+
+  def valid_anagram?(possible_anagram)
+    !same_word?(possible_anagram) && anagram_match?(possible_anagram)
   end
 
-  def matched_against_words?(possible_anagram)
+  def same_word?(possible_anagram)
     possible_anagram.downcase == subject.downcase
   end
 
   def anagram_match?(possible_anagram)
-    possible_anagram.downcase.chars.sort.join == subject.downcase.chars.sort.join
+    downcase_and_sort(possible_anagram) == downcase_and_sort(subject)
+  end
+
+  def downcase_and_sort(string)
+    string.downcase.chars.sort.join
   end
 
 end

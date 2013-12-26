6c0bb9566950a64b8aa4ea0676d4c5c816911e61
Jonah <Jonahkmoses@gmail.com>
2013-12-26 13:51:25 -0800

    passing hamming

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,45 +1,27 @@
 class Hamming
-  class << self
 
-    # def compute(input_1, input_2)
-    #   split_strands(input_1, input_2)
-    #   number_of_matching_strand_pieces
-    # end
-
-    # def split_strands(strand_1, strand_2)
-    #   @strand_1 = strand_1.split('')
-    #   @strand_2 = strand_2.split('')
-    # end
-
-    # def number_of_matching_strand_pieces
-    #   puts matching_strand_pieces
-    #   if !matching_strand_pieces.empty?
-    #     matching_strand_pieces.count
-    #   else
-    #     0
-    #   end
-    # end
-
-    # def matching_strand_pieces
-    #   @strand_1 & @strand_2
-    # end
-
-    #take both strands, split up into individual pieces
-      #done
-    #create a count that defaults to zero
-    #compare the individual pieces of each strand
-      #if they don't match +1 to the count
-
-# @array1 = ['a', 'b', 'c', 'd', 'e']
-# @array2 = ['d', 'e', 'f', 'g', 'h']
-# @intersection = @array1 & @array2
-# @intersection should now be ['d', 'e']. You can then do the following:
-
-# <% if !@intersection.empty? %>
-#   <%= @intersection.size %> Matches Found.
-# <% else %>
-#   No Matches Found.
-# <% end %>
-
+  def self.compute(input_1, input_2)
+    split_strands(input_1, input_2)
+    compare_strands
   end
+
+private
+
+  def self.split_strands(strand_1, strand_2)
+    @strand_1 = strand_1.split('')
+    @strand_2 = strand_2.split('')
+  end
+
+  def self.compare_strands
+    @strand_1.zip(@strand_2).count do |string_a, string_b|
+      self.substitution?(string_a, string_b)
+    end
+  end
+
+  def self.substitution?(string_a, string_b)
+    unless string_a == nil || string_b == nil
+      string_a != string_b
+    end
+  end
+
 end

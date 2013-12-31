4511f3c776170069951b7239a2fda9d0086f538d
Jonah <Jonahkmoses@gmail.com>
2013-12-31 15:21:55 -0800

    started binary tree

---
+++ ruby/binary-search-tree/bst.rb
@@ -0,0 +1,18 @@
+class Bst
+  attr_reader :data
+
+  def initialize(input)
+    @data = input
+  end
+
+  def insert(new_data)
+    # is new_data < or > @data
+
+    # add new_data into an array
+
+    # add @data into same array
+
+
+  end
+
+end

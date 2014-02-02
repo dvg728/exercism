ff6e8dfe605fd77d4a923c7591efa4a2d9da5602
Jonah <Jonahkmoses@gmail.com>
2014-02-01 17:15:31 -0700

    finish raindrops

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -5,34 +5,8 @@ class Grains
   end
 
   def total
-    # sum of each square, 1 - 64
-    # start with 0, add result of square method for each square
     squares = 1..64
-    
-    # sum     = 0
-    # squares.each do |s|
-    #   # take result and store it into sum
-    #   sum += square(s)
-    # end
-    # sum
-
-    # squares.inject(0) do |acc, s|
-      # acc += square(s)
-    # end
-
     squares.map{|s| square(s)}.reduce(0, &:+)
-
-    # add = ->(a, b) {a + b}
-    # squares.map{|s| square(s)}.reduce(0, &add)
-    
-
-
-
   end
 
 end
-
-
-# square count = 1..64
-# square value = 2 * the previous square count's value
-
---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,32 @@
+require 'prime'
+
+class Raindrops
+
+  def convert(number)
+    substitution_for(number) == "" ? number.to_s : substitution_for(number)
+  end
+
+private
+
+  def substitution_for(number)
+    return_string = ""
+
+    if prime_factors(number).include?(3)
+      return_string += "Pling"
+    end
+
+    if prime_factors(number).include?(5)
+      return_string += "Plang"
+    end
+
+    if prime_factors(number).include?(7)
+      return_string += "Plong"
+    end
+
+    return_string
+  end 
+
+  def prime_factors(number)
+    @prime_factors ||= Prime.prime_division(number).flatten 
+  end
+end

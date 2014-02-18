20b3affaa8e643052eca258c90930ff4fc321dc3
Jonah <Jonahkmoses@gmail.com>
2014-02-17 21:10:06 -0800

    nth-prime

--- ruby/nth_prime/prime.rb
+++ ruby/nth_prime/prime.rb
@@ -2,45 +2,45 @@ require 'prime'
 
 class Prime
 
-  def self.nth(index)
-    raise ArgumentError unless index > 0
-    primes[index]
-  end
+#   def self.nth(index)
+#     raise ArgumentError unless index > 0
+#     primes[index]
+#   end
 
-private
+# private
 
-  def self.primes
-    @primes ||= find_primes
-  end
+#   def self.primes
+#     @primes ||= find_primes
+#   end
 
-  def self.find_primes
-    possible_numbers = (2..200000).to_a
-    possible_numbers.each do |a|
-      possible_numbers.reject! do |b|
-        (a != b) && (b % a == 0)
-      end
+#   def self.find_primes
+#     possible_numbers = (2..10003).to_a
+#     possible_numbers.each do |a|
+#       possible_numbers.reject! do |b|
+#         (a != b) && (b % a == 0)
+#       end
+#     end
+#     possible_numbers.unshift(1)
+#   end
+# end
+
+  def nth(number)
+
+    if number == 0
+      raise ArgumentError.new
     end
-    possible_numbers.unshift(1)
+
+    how_many_primes = 0
+    starting_point = 1
+
+    while how_many_primes < number
+      starting_point = starting_point + 1
+
+      if Prime.prime?(starting_point)
+        how_many_primes = how_many_primes + 1
+      end
+
+    end
+    starting_point
   end
-
-  # def nth(number)
-
-  #   if number == 0
-  #     raise ArgumentError.new
-  #   end
-
-  #   how_many_primes = 0
-  #   starting_point = 1
-
-  #     while how_many_primes < number
-  #       starting_point = starting_point + 1
-
-  #       if Prime.prime?(starting_point)
-  #         how_many_primes = how_many_primes + 1
-  #       end
-
-  #     end
-  #     starting_point
-  # end
-
 end

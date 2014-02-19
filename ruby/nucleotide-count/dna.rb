04cf703f4bae77f06e0e24e3ccd11774edb56dc9
Jonah <Jonahkmoses@gmail.com>
2014-02-19 08:52:46 -0800

    WIP, Nucleotide_counts

--- ruby/nucleotide-count/dna.rb
+++ ruby/nucleotide-count/dna.rb
@@ -1,15 +1,22 @@
 class DNA
+  attr_reader :dna_string
+  attr_accessor :nucleotide_counts
 
   def initialize(dna_string)
-    @dna_string = dna_string
+    @dna_strings = dna_string.split("")
+    @nucleotide_counts ||= {'A' => 0, 'T' => 0, 'C' => 0, 'G' => 0 }
   end
 
-  def count(dna)
-    0
+  def count(dna_strand)
+    @nucleotide_counts.each do |k,v|
+
+    end
   end
 
-  def nucleotide_counts
-    {'A' => 0, 'T' => 0, 'C' => 0, 'G' => 0 }
-  end
+  # iterate through dna_string
+  # cound how many times a specific_dna strain is contained inside of the
+  # dna_string
+  # update the nucleotide_counts has with correct numbers
+
 
 end
--- ruby/nucleotide-count/nucleotide-count_test.rb
+++ ruby/nucleotide-count/nucleotide-count_test.rb
@@ -8,11 +8,13 @@ class DNATest < MiniTest::Unit::TestCase
   end
 
   def test_empty_dna_string_has_no_nucleotides
+    skip
     expected = {'A' => 0, 'T' => 0, 'C' => 0, 'G' => 0}
     assert_equal expected, DNA.new('').nucleotide_counts
   end
 
   def test_repetitive_cytidine_gets_counted
+    skip
     assert_equal 5, DNA.new('CCCCC').count('C')
   end
 

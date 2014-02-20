2e2c71629c909eae9c448c0bc0c0f3ef53c2ba79
Jonah <Jonahkmoses@gmail.com>
2014-02-20 12:21:16 -0700

    finalize nucleotides

--- ruby/nucleotide-count/dna.rb
+++ ruby/nucleotide-count/dna.rb
@@ -6,15 +6,19 @@ class DNA
 
   def initialize(molecules)
     @molecules = molecules.split("")
+    validation
+    @count = 0
+    @nucleotides = {'A' => 0, 'T' => 0, 'C' => 0, 'G' => 0 }
+    @nucleotides.default = 0
+    incrementing(@molecules)
+  end
+
+  def validation
     valid_molecules = %w(A T C G)
     validated_molecules =  @molecules.all? { |m| valid_molecules.include?(m) }
     unless validated_molecules
       raise ArgumentError
     end
-    @count = 0
-    @nucleotides = {'A' => 0, 'T' => 0, 'C' => 0, 'G' => 0 }
-    @nucleotides.default = 0
-    incrementing(@molecules)
   end
 
   def count(molecule)

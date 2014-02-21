9056bc7ad1d02a849e03147c8c8e62e98390f60e
Jonah <Jonahkmoses@gmail.com>
2014-02-21 14:06:12 -0700

    complete prime

--- ruby/nth_prime/prime.rb
+++ ruby/nth_prime/prime.rb
@@ -1,29 +1,6 @@
 require 'prime'
 
 class Prime
-
-#   def self.nth(index)
-#     raise ArgumentError unless index > 0
-#     primes[index]
-#   end
-
-# private
-
-#   def self.primes
-#     @primes ||= find_primes
-#   end
-
-#   def self.find_primes
-#     possible_numbers = (2..10003).to_a
-#     possible_numbers.each do |a|
-#       possible_numbers.reject! do |b|
-#         (a != b) && (b % a == 0)
-#       end
-#     end
-#     possible_numbers.unshift(1)
-#   end
-# end
-
   def nth(number)
 
     if number == 0

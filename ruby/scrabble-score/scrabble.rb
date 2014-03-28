3d325981a61055d507ed289be3cae3ee2d6f94f0
Jonah <Jonahkmoses@gmail.com>
2014-03-28 10:48:25 -0700

    refactor scrabble_score

--- ruby/scrabble-score/scrabble.rb
+++ ruby/scrabble-score/scrabble.rb
@@ -1,18 +1,13 @@
 class Scrabble
   attr_reader :letters
-  attr_accessor :score
 
-  def initialize(word)
-    make_individual_letters_out_of(word)
-    @score = 0
+  def self.score(word)
+    new(word).score
   end
 
-  def make_individual_letters_out_of(word)
-    if word == nil
-      word = ""
-    end
-    valid_string = word.gsub(/[\t\n ]/, "")
-    @letters = valid_string.upcase.split("")
+  def initialize(word)
+    @score = 0
+    make_individual_letters_out_of(word)
   end
 
   def score
@@ -20,9 +15,15 @@ class Scrabble
     @score
   end
 
-   def self.score(word)
-     new(word).score
-   end
+private
+
+  def make_individual_letters_out_of(word)
+    @letters = parse(word.to_s)
+  end
+
+  def parse(word)
+    word.gsub(/\s+/,"").upcase.chars
+  end
 
   def score_letters
     letters.each do |l|
@@ -31,7 +32,11 @@ class Scrabble
   end
 
   def letter_scores
-    { "A" => 1, "E" => 1, "I" => 1, "O" => 1, "U" => 1, "L" => 1, "N" => 1, "R" => 1, "S" => 1, "T" => 1, "D" => 2, "G" => 2, "B" => 3, "C" => 3, "M" => 3, "P" => 3, "F" => 4, "H" => 4, "V" => 4, "W" => 4, "Y" => 4, "K" => 5, "J" => 8, "X" => 8, "Q" => 10, "Z" => 10 }
+    {
+      "A" => 1, "E" => 1, "I" => 1, "O" => 1, "U" => 1, "L" => 1, "N" => 1, "R" => 1, "S" => 1, "T" => 1,
+      "D" => 2, "G" => 2, "B" => 3, "C" => 3, "M" => 3, "P" => 3, "F" => 4, "H" => 4, "V" => 4, "W" => 4,
+      "Y" => 4, "K" => 5, "J" => 8, "X" => 8, "Q" => 10, "Z" => 10
+    }
   end
 
 end

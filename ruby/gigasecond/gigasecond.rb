ec50cc632db304818cec7c723981decfe7533d54
Jonah Moses <jmoses@newrelic.com>
2014-10-29 16:30:54 -0700

    gigasecond, raindrops, rna

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,8 +1,11 @@
 class Gigasecond
-  attr_reader :date
 
-  def initialize(date)
-    @date = date
+  def initialize(birthday)
+    @birthday = birthday
+  end
+
+  def date
+    @birthday + Rational(10**9/86400)
   end
 
 end

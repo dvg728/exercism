bde47d5745d3e0d76303d6863428704dd7d1332e
Jonah Moses <jonahkmoses@gmail.com>
2014-11-27 02:04:26 -0800

    remove whitespace

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -1,6 +1,6 @@
 class Complement
 
-  COMPLEMENTS = { 
+  COMPLEMENTS = {
     'C' => 'G',
     'G' => 'C',
     'T' => 'A',
@@ -8,13 +8,13 @@ class Complement
   }
 
   def self.of_dna(nucleotide)
-    nucleotide.chars.map do |n| 
+    nucleotide.chars.map do |n|
       COMPLEMENTS[n]
     end.join
   end
 
   def self.of_rna(nucleotide)
-    nucleotide.chars.map do |n| 
+    nucleotide.chars.map do |n|
       COMPLEMENTS.invert[n]
     end.join
   end
